6ecd3b2a415bcf24aac9a16d49c5fb47609ad2ff
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2023-03-27 15:25:21 +0200

    Aggiungere i file di progetto.

---
+++ firstProgram/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Hello, I'm gonna show you some data types: \n");
+
+string[] types = {"String", "Char", "Int", "Decimal", "Bool"};
+
+for (int i = 1; i <= types.Length; i++)
+{
+    Console.WriteLine($"{i} - { types[i - 1]}");
+}
+
+
+
